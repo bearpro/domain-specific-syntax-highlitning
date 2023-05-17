9be3c17eda8b5545105e44d80bc53a27b0a096a6
Михаил Проказин <bearpro@outlook.com>
2023-05-17 09:59:41 +0300

    Added semantic token modifiers

--- server/SemanticTokensHandler.cs
+++ server/SemanticTokensHandler.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Microsoft.Extensions.Logging;
 using OmniSharp.Extensions.LanguageServer.Protocol;
 using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
@@ -22,13 +23,21 @@ public class SemanticTokensHandler : SemanticTokensHandlerBase
             DocumentSelector = DocumentSelector.ForPattern("**/*.smpl.dssh"),
             Legend = new SemanticTokensLegend
             {
-                // TokenTypes = capability.TokenTypes,
                 TokenTypes = new Container<SemanticTokenType>(
                     SemanticTokenType.Comment,
                     SemanticTokenType.Keyword,
-                    SemanticTokenType.Function
+                    SemanticTokenType.Function,
+                    SemanticTokenType.Method,
+                    SemanticTokenType.String,
+                    SemanticTokenType.Variable
                 ),
-                TokenModifiers = capability.TokenModifiers
+                TokenModifiers = new Container<SemanticTokenModifier>(
+                    "sql-captured",
+                    "concurrency-captured",
+                    "concurrency-modified",
+                    "ui-captured",
+                    SemanticTokenModifier.Async
+                )
             },
             Full = true
         };
@@ -58,22 +67,125 @@ public class SemanticTokensHandler : SemanticTokensHandlerBase
 
         foreach (var (lineContent, lineNumber) in content.Select((l, i) => (l, i)))
         {
+            var queue = new PriorityQueue<(int, int, string, string[]), int>();
+
             if (lineContent.StartsWith('#'))
             {
-                builder.Push(
-                    line: lineNumber, 
-                    @char: 0, 
-                    length: lineContent.Length, 
-                    tokenType: (string)SemanticTokenType.Comment);
+                queue.Enqueue((
+                    0,
+                    lineContent.Length,
+                    SemanticTokenType.Comment,
+                    Array.Empty<string>()
+                ),
+                0);
             }
-            else if (lineContent.StartsWith("do"))
+
+            var foundKeywords = FindKeywords(lineContent, new [] {"var", "do", "fun"});
+            foreach (var (keywordStart, keywordLen, _) in foundKeywords)
             {
+                queue.Enqueue((
+                    keywordStart,
+                    keywordLen,
+                    SemanticTokenType.Keyword,
+                    Array.Empty<string>()),
+                    keywordStart);
+            }
+
+            var stringMatches = Regex.Matches(lineContent, "\".*?\"", RegexOptions.Singleline);
+            foreach (var match in stringMatches.Select(x => x))
+            {
+                queue.Enqueue((
+                    match.Index,
+                    match.Length,
+                    SemanticTokenType.String,
+                    Array.Empty<string>()),
+                    match.Index);
+            }
+            
+            var funcCallMatches = Regex.Matches(lineContent, "\\w+?\\(", RegexOptions.Singleline);
+            foreach (var match in funcCallMatches.Select(x => x))
+            {
+                queue.Enqueue((
+                    match.Index,
+                    match.Length-1,
+                    SemanticTokenType.Method,
+                    Array.Empty<string>()), 
+                    match.Index);
+            }
+
+            var foundVariables = FindKeywords(lineContent, new [] {
+                "shared_counter",
+                "increment",
+                "counter_label",
+                "counterId",
+                "sql_session",
+                "ui"});
+            foreach (var (keywordStart, keywordLen, variable) in foundVariables)
+            {
+                string[] modifiers = variable switch 
+                {
+                    "shared_counter" => new[] { "concurrency-modified", "concurrency-captured", "ui-captured", "sql-captured" },
+                    "increment" => new[] { "concurrency-captured" },
+                    "counter_label" => new[] { "ui-captured" },
+                    "counterId" => new[] { "sql-captured" },
+                    "ui" => Array.Empty<string>(),
+                    _ => Array.Empty<string>()
+                };
+
+                queue.Enqueue((
+                    keywordStart,
+                    keywordLen,
+                    SemanticTokenType.Variable,
+                    modifiers),
+                    keywordStart);
+            }
+
+            while (queue.TryDequeue(out var item, out var priority))
+            {
+                var (start, len, token, modifiers) = item;
                 builder.Push(
-                    line: lineNumber,
-                    @char: 0,
-                    length: 2,
-                    tokenType: (string)SemanticTokenType.Keyword);
+                    lineNumber,
+                    start,
+                    len,
+                    token,
+                    modifiers);
             }
         }
+    }
+
+    protected List<(int, int, string)> FindKeywords(
+        string line, 
+        string[] keywords, 
+        int startFrom = 0,
+        List<(int, int, string)>? foundKeywords = null)
+    {
+        foundKeywords ??= new();
+
+        var keywordIndex = -1;
+        var keywordLenght = 0;
+        string? foundKeyword = null;
+
+        foreach (var keyword in keywords)
+        {
+            var index = line.IndexOf(keyword, startIndex: startFrom);
+            if (index != -1)
+            {
+                keywordIndex = index;
+                keywordLenght = keyword.Length;
+                foundKeyword = keyword;
+                break;
+            }
+            continue;
+        }
+
+        if (keywordIndex == -1)
+        {
+            return foundKeywords;
+        }
+
+        startFrom = keywordIndex + keywordLenght;
+        foundKeywords.Add((keywordIndex, keywordLenght, foundKeyword!));
+        
+        return FindKeywords(line, keywords, startFrom, foundKeywords);
     }
 }
